c7061df786b564b13ceb2c2a2ad4451c7eaa4712
bautistamaxine3002@gmail.com <[bautistamaxine3002@gmail.com]>
2024-07-18 13:49:25 +0800

    add indext.php

--- indext.php
+++ indext.php
@@ -1,2 +1,13 @@
-
-mhjghfgvjhfjhfyhgfhgfg
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>docs</title>
+</head>
+<body>
+<h1>Maxine Bautista</h1>
+<h2>Block 3</h2>
+<h3>Third Year summer</h3>
+</body>
+</html>
